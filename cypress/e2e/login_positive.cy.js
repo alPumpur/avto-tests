d17fb8b2b54312f9fbcb8cf87c5d0dddc17a127e
alPumpur <aleftinapumpur@gmail.com>
2024-05-06 20:11:37 +0700

    тест для регистрации(позитивный)

--- cypress/e2e/login_positive.cy.js
+++ cypress/e2e/login_positive.cy.js
@@ -2,7 +2,7 @@ describe('template srec', () => {
     it('passed', () => {
         cy.visit('https://dev.profteam.su/login')
     })
-    it('Existent login test', () => {
+    it('Positive login test', () => {
         cy.fixture('data_test_login').then(data => {
 
             cy.visit('https://dev.profteam.su/login')
@@ -18,9 +18,24 @@ describe('template srec', () => {
 
             cy.log('Клик по кнопке "Войти"')
             cy.get(':nth-child(3) > .button').click()
+        })
+    })
+    it('Negative login test', () => {
+        cy.fixture('data_test_login_n').then(data => {
 
+            cy.visit('https://dev.profteam.su/login')
+            cy.log('Переход на страницу авторизации')
 
+            cy.log('Ввод правильного логина')
+            cy.get('.form-input--text')
+                .type(data.login)
 
+            cy.log('Ввод неправильного пароля')
+            cy.get('.form-input--password')
+                .type(data.password)
+
+            cy.log('Клик по кнопке "Войти"')
+            cy.get(':nth-child(3) > .button').click()
         })
     })
 })
