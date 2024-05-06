dd1b633ce33d351fc5db9fe8137864a1dc116691
alPumpur <aleftinapumpur@gmail.com>
2024-05-06 10:53:35 +0700

    тест на проверку корректной авторизации(негативный)

--- cypress/e2e/login.negative.cy.js
+++ cypress/e2e/login.negative.cy.js
@@ -3,7 +3,7 @@ describe('template srec', () => {
         cy.visit('https://dev.profteam.su/login')
     })
     it('Existent login test', () => {
-        cy.fixture('data_test_login').then(data => {
+        cy.fixture('data_test_login_n').then(data => {
 
             cy.visit('https://dev.profteam.su/login')
             cy.log('Переход на страницу авторизации')
@@ -12,7 +12,7 @@ describe('template srec', () => {
             cy.get('.form-input--text')
                 .type(data.login)
 
-            cy.log('Ввод правильного пароля')
+            cy.log('Ввод неправильного пароля')
             cy.get('.form-input--password')
                 .type(data.password)
 
