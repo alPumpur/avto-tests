291e8e6572f7c0926cba8f579c0a99b80947a59e
alPumpur <aleftinapumpur@gmail.com>
2024-05-07 12:11:37 +0700

    Добавление тестов для стажировки

---
+++ cypress/e2e/work_search.cy.js
@@ -0,0 +1,27 @@
+describe('template spec', () => {
+    it('Working test', () => {
+        cy.fixture('data_test_login').then(data =>{
+            cy.visit('https://dev.profteam.su/login')
+
+            cy.log('Ввод логина')
+            cy.get('.form-input--text').type(data.login)
+
+            cy.log('Ввод пароля')
+            cy.get('.form-input--password').type(data.password)
+
+            cy.log('Клик по кнопке')
+            cy.get('.form__buttons > :nth-child(3)').click()
+            cy.wait(2000)
+
+            cy.log('Клик по кнопке "Стажировки"')
+            cy.get(':nth-child(1) > .header__nav > [href="/internships"]').click()
+            cy.wait(2000)
+
+            cy.log('Ввод запроса')
+            cy.get('.form-input--text').type(data.search)
+
+            cy.log('Клик по иконке поиска')
+            cy.get('div.search-input__field > .button').click()
+        })
+    })
+})
